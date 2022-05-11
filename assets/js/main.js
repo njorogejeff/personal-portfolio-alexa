4ca14e0b424492706d25ab4fe80a86f5699571ac
njorogejeff <njorogejeff254@gmail.com>
2022-05-11 07:33:16 +0300

    website JavaScript functionality file

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,7 +95,7 @@ modalCloses.forEach((modalClose) => {
 
 
 // PORTFOLIO SWIPER
-let swiper = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
     cssMode: true,
     loop: true,
 
@@ -107,4 +107,22 @@ let swiper = new Swiper('.portfolio__container', {
         el: '.swiper-pagination',
         clickable: true,
     },
+});
+
+// TESTIMONIAL SWIPER
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+        }
+    }
 });
